0946d4bc2751c35252ffebcf3fd98a78a12581c0
eqbal <eqbal.quran@gmail.com>
2014-05-16 02:32:49 +0300

    Modifying the main page and actions related

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -28,6 +28,7 @@ class EntriesController < ApplicationController
   # POST /entries.json
   def create
     @entry = Entry.new(entry_params)
+    @entry.user_id = current_user.id
 
     respond_to do |format|
       if @entry.save
