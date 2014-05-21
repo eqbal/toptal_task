e78ec222d57c48f4d726e7aeeb3e8dee55811a09
eqbal <eqbal.quran@gmail.com>
2014-05-21 06:38:52 +0300

    Adding helpers(filter) to check if the user has an access key

--- app/controllers/api/v1/base_controller.rb
+++ app/controllers/api/v1/base_controller.rb
@@ -1,8 +1,34 @@
+include ActionController::HttpAuthentication::Token::ControllerMethods
+include ActionController::MimeResponds
+
 class Api::V1::BaseController < ActionController::Base
   
   respond_to :json
 
+  private
 
+  def restrict_access
+    unless restrict_access_by_params || restrict_access_by_header
+      render json: {message: 'Invalid API Token'}, status: 401
+      return
+    end
+
+    @current_user = @api_key.user if @api_key
+  end
+
+  def restrict_access_by_header
+    return true if @api_key
+
+    authenticate_with_http_token do |token|
+      @api_key = ApiKey.find_by_token(token)
+    end
+  end
+
+  def restrict_access_by_params
+    return true if @api_key
+
+    @api_key = ApiKey.find_by_token(params[:token])
+  end
 
 
 end
--- app/controllers/api/v1/entries_controller.rb
+++ app/controllers/api/v1/entries_controller.rb
@@ -1,4 +1,5 @@
 class Api::V1::EntriesController < Api::V1::BaseController
+  before_filter :restrict_access 
 
   def index
   	@entries = Entry.all
